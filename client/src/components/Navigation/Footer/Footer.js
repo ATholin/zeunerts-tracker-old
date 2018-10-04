3e88ee955fa935f3eb0a73a125df6bf809f13361
Alexander Andersson Tholin <alexander.a.tholin@gmail.com>
2018-10-04 12:44:47 +0200

    footer fix

--- client/src/components/Navigation/Footer/Footer.js
+++ client/src/components/Navigation/Footer/Footer.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React from "react";
 
-import './Footer.css'
+import "./Footer.css";
 
 const Footer = () => (
-    <div className="w-full absolute pin-b text-center mx-auto pb-6">
-        <a rel="noopener noreferrer" target="_blank" href="https://github.com/ATholin/zeunerts-tracker">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-10 w-10 feather feather-github text-grey-darkest"><path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path></svg>
-        </a>
-        <p className="pt-4 pb-3 text-grey-darker">Skapad av <a rel="noopener noreferrer" target="_blank" href="https://github.com/ATholin" className="text-black">@ATholin</a></p>
-    </div>
+  <div className="w-full absolute pin-b text-center mx-auto pb-6">
+    <a
+      rel="noopener noreferrer"
+      target="_blank"
+      href="https://github.com/ATholin/zeunerts-tracker"
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="h-10 w-10 feather feather-github text-grey-darkest"
+      >
+        <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22" />
+      </svg>
+    </a>
+    <p className="pt-4 pb-3 text-grey-darker">
+      Skapad av{" "}
+      <a
+        rel="noopener noreferrer"
+        target="_blank"
+        href="https://github.com/ATholin"
+        className="text-black"
+      >
+        ATholin
+      </a>
+    </p>
+  </div>
 );
 
-export default Footer
+export default Footer;
