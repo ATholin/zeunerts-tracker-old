c5a2fbe9b3b1ac743dcd05d662534955adf692db
Alexander Andersson Tholin <alexander.a.tholin@gmail.com>
2018-10-06 19:45:05 +0200

    index: Added padding to main container to account for mobile resolution

--- client/src/hoc/Layout/Layout.js
+++ client/src/hoc/Layout/Layout.js
@@ -1,35 +1,32 @@
-import React, { Component } from 'react';
-import Button from '@material-ui/core/Button';
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
 
-import Aux from '../auxWrapper'
-import Navbar from '../../components/Navigation/Navbar/Navbar';
-import Footer from '../../components/Navigation/Footer/Footer'
+import Aux from "../auxWrapper";
+import Navbar from "../../components/Navigation/Navbar/Navbar";
+import Footer from "../../components/Navigation/Footer/Footer";
 
 class Layout extends Component {
-    render() {
-        const showFab=window.location.pathname === '/'
-        return (
-            <Aux>
-                <Navbar />
-                <main className="container mx-auto">
-                    {this.props.children}
-                </main>
-                {
-                    showFab
-
-
-                        ? (
-                            < Link className="no-underline m-10 fixed pin-r pin-b z-50" to="/add">
-                                <div className='fab'> <Button className='fab hover:shadow-lg' variant='fab'>+</Button>
-                                </div>
-                            </Link>)
-                        : null
-                }
-                <Footer />
-            </Aux >
-        );
-    }
+  render() {
+    const showFab = window.location.pathname === "/";
+    return (
+      <Aux>
+        <Navbar />
+        <main className="container mx-auto px-3">{this.props.children}</main>
+        {showFab ? (
+          <Link className="no-underline m-10 fixed pin-r pin-b z-50" to="/add">
+            <div className="fab">
+              {" "}
+              <Button className="fab hover:shadow-lg" variant="fab">
+                +
+              </Button>
+            </div>
+          </Link>
+        ) : null}
+        <Footer />
+      </Aux>
+    );
+  }
 }
 
 export default Layout;
