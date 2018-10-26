737fb730ab9b87c4437194a60605be2300fc9cef
Alexander Andersson Tholin <alexander.a.tholin@gmail.com>
2018-10-26 20:41:55 +0200

    https middleware

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ const near = require("./routes/api/near");
 
 const app = express();
 
+// Force https
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (
+    !req.secure &&
+    req.get("x-forwarded-proto") !== "https" &&
+    process.env.NODE_ENV !== "development"
+  ) {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+app.use(requireHTTPS);
+
 //Bodyparser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
